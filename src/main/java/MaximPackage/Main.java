0b24410b651a69443f30d3d9535993248ade594e
Maksims <maksims.puskels@gmail.com>
2015-10-19 07:41:30 +0300

    Main class added.

---
+++ src/main/java/MaximPackage/Main.java
@@ -0,0 +1,11 @@
+package MaximPackage;
+
+/**
+ * Created by maksimspuskels on 19/10/15.
+ */
+public class Main {
+    public static void main(String[] args){
+        User user = new User(1, "Tourist", "t@email.com", "Riga", "Latvia");
+
+    }
+}
