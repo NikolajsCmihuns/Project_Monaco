db0f395c223edfa5b8e8608b5bea294f63c7aba1
Janis Karklins <jjankar@gmail.com>
2015-11-12 12:09:29 +0200

    App

---
+++ src/main/java/janis/monaco/app/App.java
@@ -0,0 +1,33 @@
+package janis.monaco.app;
+
+
+import janis.monaco.domain.Place;
+import janis.monaco.util.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+/**
+ * Created by janis on 11/2/15.
+ */
+public class App {
+    public static void main(String[] args){
+//Create session factory object
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        //getting session object from session factory
+        Session session = sessionFactory.openSession();
+        //getting transaction object from session object
+        session.beginTransaction();
+        Query query = session.createQuery("from Place ");
+        List<Place> places = query.list();
+        for(Place place : places)
+        {
+            System.out.println("id: " + place.getPlaceId() + ", Name: " + place.getPlaceName() + ", Address: " + place.getPlaceAddress());
+        }
+        session.getTransaction().commit();
+        sessionFactory.close();
+    }
+}
+
