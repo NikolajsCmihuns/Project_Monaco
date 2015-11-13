f3df78ac09217292598158682bebbd75f2007cd1
narlot <aleksandr.antonuk@gmail.com>
2015-11-13 17:55:45 +0200

    Project Monako Console Test Upload

--- src/main/java/AlexPackage/RouteController.java
+++ src/main/java/AlexPackage/RouteController.java
@@ -6,7 +6,5 @@ import javax.servlet.http.HttpServletRequest;
  * Created by Aborigens on 05-Nov-15.
  */
 public interface RouteController {
-
     RouteModel execute(HttpServletRequest request);
-
 }
