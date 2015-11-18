b0e33ae28b11b56c7506890b93f70a80b0b3eda4
Janis Karklins <jjankar@gmail.com>
2015-11-18 23:04:41 +0200

    PlaceDAO

---
+++ src/main/java/janis/monaco/domain/dao/PlaceDAO.java
@@ -0,0 +1,27 @@
+package janis.monaco.domain.dao;
+
+import janis.monaco.domain.entities.Place;
+
+/**
+ * Created by janis on 11/16/15.
+ */
+
+public class PlaceDAO {
+
+
+
+    public Place creatPlace(Place place){
+
+
+
+        return place;
+
+    }
+
+    public void updatePlace(){
+
+    }
+
+
+
+}
