dba3fa4e56f085bbda04db746494a3ece4718acc
Janis Karklins <jjankar@gmail.com>
2015-11-19 19:52:36 +0200

    Spring and mvc

---
+++ src/main/java/janis/monaco/controllers/ControllerPlace.java
@@ -0,0 +1,13 @@
+package janis.monaco.controllers;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by janis on 11/19/15.
+ */
+
+@Component
+public class ControllerPlace {
+
+
+}
