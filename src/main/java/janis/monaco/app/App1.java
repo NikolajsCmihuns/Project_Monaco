fcee402e2e5ed3c87c5da4fc1080daeae0972411
Janis Karklins <jjankar@gmail.com>
2015-11-19 12:44:23 +0200

    App1 for testing implementation

---
+++ src/main/java/janis/monaco/app/App1.java
@@ -0,0 +1,37 @@
+package janis.monaco.app;
+
+
+import janis.monaco.database.CRUDOperationDAO;
+import janis.monaco.database.place.PlaceDAOImpl;
+import janis.monaco.database.PlaceDAO;
+import janis.monaco.domain.entities.Place;
+import org.hibernate.Query;
+
+import java.util.List;
+
+
+/**
+ * Created by janis on 11/2/15.
+ */
+public class App1 {
+    public static void main(String[] args){
+
+
+
+
+        PlaceDAO pl = new PlaceDAOImpl();
+
+        List<Place> places = pl.getAll();
+
+        for(Place place : places)
+        {
+            System.out.println("id: " + place.getPlaceId() + ", Name: " + place.getPlaceName() + ", Address: " + place.getPlaceAddress());
+
+        }
+
+
+
+
+    }
+}
+
