c1e3a04bfb5bb48ef1356e8c6bf52d4b96e03090
Janis Karklins <jjankar@gmail.com>
2015-11-20 11:08:42 +0200

    placeTypeRef added

---
+++ src/main/java/janis/monaco/database/placetyperef/PlaceTypeRefDAOImpl.java
@@ -0,0 +1,9 @@
+package janis.monaco.database.placetyperef;
+
+
+import janis.monaco.database.PlaceTypeRefDAO;
+import janis.monaco.domain.entities.PlaceTypeRef;
+
+public class PlaceTypeRefDAOImpl extends CRUDOperationDAOImpl<PlaceTypeRef> implements PlaceTypeRefDAO {
+
+}
