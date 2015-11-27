88549ec7f6eb85ed4b289d6bbc73380a703159fd
Janis Karklins <jjankar@gmail.com>
2015-11-27 10:33:10 +0200

    Refactoring file

--- src/main/java/janis/monaco/config/SpringConfig.java
+++ src/main/java/janis/monaco/config/SpringConfig.java
@@ -1,12 +1,16 @@
 package janis.monaco.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by janis on 11/19/15.
  */
 
 @ComponentScan(basePackages = {"janis.monaco"})
+@Configuration
 public class SpringConfig {
+
+
 
 }
--- src/main/java/janis/monaco/database/place/PlaceDAOImpl.java
+++ src/main/java/janis/monaco/database/place/PlaceDAOImpl.java
@@ -3,7 +3,10 @@ package janis.monaco.database.place;
 
 import janis.monaco.database.PlaceDAO;
 import janis.monaco.domain.Place;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class PlaceDAOImpl extends CRUDOperationDAOImpl<Place> implements PlaceDAO {
 
 }
