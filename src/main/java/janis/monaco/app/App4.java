017329991cd1dcda78abbaf4c45272c2a2bd03c2
Janis Karklins <jjankar@gmail.com>
2015-12-01 18:55:17 +0200

    PlaceConsole

---
+++ src/main/java/janis/monaco/app/App4.java
@@ -0,0 +1,38 @@
+package janis.monaco.app;
+
+
+import janis.monaco.database.PlaceDAO;
+import janis.monaco.database.place.PlaceDAOImpl;
+import janis.monaco.domain.Place;
+
+import java.util.List;
+
+
+/**
+ * Created by janis on 11/2/15.
+ */
+public class App4 {
+    public static void main(String[] args){
+
+
+
+
+        PlaceDAO pl = new PlaceDAOImpl();
+
+
+        List<Place> places = pl.getAll();
+
+
+
+        for(Place place : places)
+        {
+            System.out.println("id: " + place.getPlaceId() + ", Name: " + place.getPlaceName() + ", Address: " + place.getPlaceAddress());
+
+        }
+
+
+
+
+    }
+}
+
