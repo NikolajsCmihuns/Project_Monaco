2ca632e372c98ffce04fdef2426752d4498ef153
Janis Karklins <jjankar@gmail.com>
2015-12-07 11:26:15 +0200

    ControllerPlace jsp classPath changed

--- src/main/java/janis/monaco/controllers/ControllerPlaceImpl.java
+++ src/main/java/janis/monaco/controllers/ControllerPlaceImpl.java
@@ -37,6 +37,6 @@ public class ControllerPlaceImpl implements ControllerPlace {
             System.out.println("The places list lenght is: "+places.size());
 
 
-        return new MVCModel("Login page here", "/place.jsp");
+        return new MVCModel("Login page here", "views/place/place.jsp");
     }
 }
