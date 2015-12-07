55fd89f2df5fa2b27dbb53bc67cdce0346468b66
Janis Karklins <jjankar@gmail.com>
2015-12-07 10:00:47 +0200

    adding interface to ControllerPlace

--- src/main/java/janis/monaco/controllers/ControllerPlace.java
+++ src/main/java/janis/monaco/controllers/ControllerPlace.java
@@ -1,22 +1,9 @@
 package janis.monaco.controllers;
 
-
 import janis.monaco.mvc.MVCController;
-import janis.monaco.mvc.MVCModel;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
- * Created by janis on 11/19/15.
+ * Created by janis on 12/7/15.
  */
-
-@Component
-public class ControllerPlace implements MVCController {
-
-
-    @Override
-    public MVCModel execute(HttpServletRequest request) {
-        return new MVCModel("Login page here", "/place.jsp");
-    }
+public interface ControllerPlace extends MVCController {
 }
---
+++ src/main/java/janis/monaco/controllers/ControllerPlaceImpl.java
@@ -0,0 +1,20 @@
+package janis.monaco.controllers;
+
+import janis.monaco.mvc.MVCModel;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by janis on 11/19/15.
+ */
+
+@Component
+public class ControllerPlaceImpl implements ControllerPlace {
+
+
+    @Override
+    public MVCModel execute(HttpServletRequest request) {
+        return new MVCModel("Login page here", "/place.jsp");
+    }
+}
