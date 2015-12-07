612a1c06f3b21b77f9e6f26224d5614fda7f9ec7
Janis Karklins <jjankar@gmail.com>
2015-12-07 11:03:17 +0200

    ControllerPlace adding bisnesslogic

--- src/main/java/janis/monaco/controllers/ControllerPlaceImpl.java
+++ src/main/java/janis/monaco/controllers/ControllerPlaceImpl.java
@@ -1,20 +1,42 @@
 package janis.monaco.controllers;
 
+import janis.monaco.database.PlaceDAO;
+import janis.monaco.domain.Place;
 import janis.monaco.mvc.MVCModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by janis on 11/19/15.
  */
 
-@Component
+@Controller
 public class ControllerPlaceImpl implements ControllerPlace {
 
+    @Autowired
+    PlaceDAO placeDAO;
 
-    @Override
+    @Transactional
     public MVCModel execute(HttpServletRequest request) {
+
+        List<Place> places = null;
+        System.out.println("ControllerPlace is called!!!");
+
+            places = placeDAO.getAll();
+
+            for(Place place : places)
+            {
+                System.out.println("id: " + place.getPlaceId() + ", Name: " + place.getPlaceName() + ", Address: " + place.getPlaceAddress());
+
+            }
+            System.out.println("The places list lenght is: "+places.size());
+
+
         return new MVCModel("Login page here", "/place.jsp");
     }
 }
