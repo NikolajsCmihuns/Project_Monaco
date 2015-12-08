eda8b23de54648bdd374114445c580783a0c7d83
Janis Karklins <jjankar@gmail.com>
2015-12-08 15:46:40 +0200

    adding logic to ControllerPlace nad bind to place.jsp

--- src/main/java/janis/monaco/controllers/ControllerPlaceImpl.java
+++ src/main/java/janis/monaco/controllers/ControllerPlaceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,15 +28,39 @@ public class ControllerPlaceImpl implements ControllerPlace {
         System.out.println("ControllerPlace is called!!!");
 
         List<Place>  places = placeDAO.getAll();
+        int size = places.size();
+        String [] placeIdStrArr = new String[size];
+        String [] latitudeStrArr = new String[size];
+        String [] longitudeStrArr = new String[size];
+        String [] imageURLStrArr = new String[size];
+        String [] placeNameStrArr = new String[size];
+        String [] placeAddressStrArr = new String[size];
+        String [] placeTypeIdStrArr = new String[size];
+        List placeList = new ArrayList();
+        int counter = 0;
 
             for(Place place : places)
             {
-                System.out.println("id: " + place.getPlaceId() + ", Name: " + place.getPlaceName() + ", Address: " + place.getPlaceAddress());
+                counter++;
+                placeIdStrArr [counter-1] = String.valueOf(place.getPlaceId());
+                latitudeStrArr [counter-1] = String.valueOf(place.getLatitude());
+                longitudeStrArr [counter-1] = String.valueOf(place.getLongitude());
+                imageURLStrArr [counter-1] = place.getImageUrl();
+                placeNameStrArr [counter-1] = place.getPlaceName();
+                placeAddressStrArr [counter-1] = place.getPlaceAddress();
+                placeTypeIdStrArr [counter-1] = String.valueOf(place.getPlaceTypeId());
 
             }
-            System.out.println("The places list lenght is: "+places.size());
+
+        placeList.add(placeIdStrArr);
+        placeList.add(latitudeStrArr);
+        placeList.add(longitudeStrArr);
+        placeList.add(imageURLStrArr);
+        placeList.add(placeNameStrArr);
+        placeList.add(placeAddressStrArr);
+        placeList.add(placeTypeIdStrArr);
 
 
-        return new MVCModel("Login page here", "/views/place_views/place.jsp");
+        return new MVCModel(placeList, "/views/place_views/place.jsp");
     }
 }
--- src/main/webapp/views/place_views/place.jsp
+++ src/main/webapp/views/place_views/place.jsp
@@ -1,3 +1,6 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+
 <%--
   Created by IntelliJ IDEA.
   User: janis
@@ -5,12 +8,29 @@
   Time: 8:20 PM
   To change this template use File | Settings | File Templates.
 --%>
+
+
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>OK</title>
+
+
+
+
+
 </head>
 <body>
-Spring !!!!!
+
+<table>
+  <c:forEach var= "place" items='${model}' >
+    <c:forEach var= "properties" items='${place}' >
+      <tr>
+        <td>${properties}</td>
+      </tr>
+    </c:forEach>
+  </c:forEach>
+</table>
 </body>
 </html>
