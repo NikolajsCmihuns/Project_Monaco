50ed69e61419afd4af41d46735dc12b0fbf90626
Janis Karklins <jjankar@gmail.com>
2015-12-22 19:37:18 +0200

    add to spring TypeDAO

--- src/main/java/janis/monaco/database/placetyperef/PlaceTypeRefDAOImpl.java
+++ src/main/java/janis/monaco/database/placetyperef/PlaceTypeRefDAOImpl.java
@@ -3,7 +3,9 @@ package janis.monaco.database.placetyperef;
 
 import janis.monaco.database.PlaceTypeRefDAO;
 import janis.monaco.domain.PlaceTypeRef;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PlaceTypeRefDAOImpl extends CRUDOperationDAOImpl<PlaceTypeRef> implements PlaceTypeRefDAO {
 
 }
