6bf5f06af393f61c8ed7a1ea47fa4d130df472d2
Janis Karklins <jjankar@gmail.com>
2016-01-14 13:54:46 +0200

    PlaceRezultControllerImpl is logic added, but no placyTypeID

--- src/main/java/janis/monaco/controllers/PlaceRezulControllertImpl.java
+++ src/main/java/janis/monaco/controllers/PlaceRezulControllertImpl.java
@@ -1,9 +1,13 @@
 package janis.monaco.controllers;
 
+import janis.monaco.database.PlaceDAO;
+import janis.monaco.domain.Place;
 import janis.monaco.mvc.MVCModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 
 /**
  * Created by janis on 12/22/15.
@@ -12,12 +16,23 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class PlaceRezulControllertImpl implements PlaceRezultController {
 
+    @Autowired
+    PlaceDAO placeDAO;
 
+    @Transactional
     public MVCModel execute(HttpServletRequest request) {
 
         System.out.println("ControllerPlaceRezult is called!!!");
 
+        Place place = new Place();
+        place.setLongitude(Double.valueOf(request.getParameter("longitude")));
+        place.setLatitude(Double.valueOf(request.getParameter("latitude")));
+        place.setImageUrl(request.getParameter("imageURL"));
+        place.setPlaceTypeId(210);
+        place.setPlaceName(request.getParameter("placeName"));
+        place.setPlaceAddress(request.getParameter("address"));
 
+        placeDAO.create(place);
 
         System.out.println(request.getParameter("placeName"));
         System.out.println(request.getParameter("placeTypeId"));
